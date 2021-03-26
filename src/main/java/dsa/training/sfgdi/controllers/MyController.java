6e6f28461d9834033bade2073d6322700429bfd4
Daniel Andrei <dandrei@sabaltech.com>
2021-03-26 22:40:56 +0200

    - DI basics

---
+++ src/main/java/dsa/training/sfgdi/controllers/MyController.java
@@ -0,0 +1,14 @@
+package dsa.training.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+  public String sayHello() {
+    System.out.println("Hello world!!!");
+
+    return "Hi Folks!";
+  }
+
+}
