2b80b11c7e581a041be0e33c79291b0ae597c095
Daniel Andrei <dandrei@sabaltech.com>
2021-03-26 22:23:04 +0200

    - initialized spring project

---
+++ src/test/java/dsa/training/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package dsa.training.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
