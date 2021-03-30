6be05df44a9e8df97458aded94e9002603d122b0
Daniel Andrei <daniel.v.andrei@softvision.com>
2021-03-30 10:11:57 +0300

    added java configuration example

---
+++ src/main/java/dsa/training/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,26 @@
+package dsa.training.sfgdi.config;
+
+import dsa.training.sfgdi.services.ConstructorGreetingService;
+import dsa.training.sfgdi.services.PropertyInjectedGreetingService;
+import dsa.training.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+}
